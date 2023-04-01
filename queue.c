361e6da759d7007418957a6a5a86e35fcfcb7d8e
Mohamed Nasser <75505750+iammohamednasser@users.noreply.github.com>
2023-04-01 14:20:36 +0000

    starting data c structers project

---
+++ queue.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+// cyclic queue using
+// array implementation
+
+typedef struct {
+    int* base;
+    int rear;
+    int front;
+    int size;
+} queue_t;
+
+queue_t* create_queue(int size) {
+    queue_t* qu;
+    qu = (queue_t*)malloc(sizeof(qu));
+    qu->base = (int*)malloc((size+1)*sizeof(int));
+    qu->rear = 0;
+    qu->front = 0;
+    qu->size = size;
+    return qu;
+}
+
+int is_empty(queue_t* qu) {
+    return (qu->rear == qu->front);
+}
+
+int dequeue(queue_t* qu) {
+    if (is_empty(qu)) {
+        printf("is empty\n");
+        return -1;
+    }
+    int temp;
+    temp = qu->front;
+    qu->front = (qu->front +1)%(qu->size+1);
+    return qu->base[temp];
+}
+
+int is_full(queue_t* qu) {
+    return ((qu->rear +1)%(qu->size+1) == qu->front);
+}
+
+void enqueue(queue_t* qu, int x) {
+    if (is_full(qu) == 1) {
+        printf("is full\n");
+        return;
+    }
+    qu->base[qu->rear] = x;
+    qu->rear = (qu->rear +1)%(qu->size+1);
+}
+
+int length(queue_t* qu) {
+    return abs(qu->rear - qu->front);
+}
+
+int main() {
+    printf("data structures");
+    return 0;
+}
